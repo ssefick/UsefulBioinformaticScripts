6f860fdcdb9eb7c9fc707147f1c1f4dfd84991cb
Stephen Sefick <ssefick@localhost.localdomain>
2016-08-04 11:59:16 -0500

    Fix comma between node:ppn and mem

--- HPC_PBS_Header.py
+++ HPC_PBS_Header.py
@@ -15,7 +15,7 @@ Example usage:
 Argument 1: Email for job status
 e.g., email@whatever.com
 
-Argument 2: Script name to write out 
+Argument 2: Script name to write out
 e.g., yourscript.sh
 
 HPC_PBS_Header.py email@whatever.com yourscript.sh
@@ -27,20 +27,20 @@ HPC_PBS_Header.py email@whatever.com yourscript.sh
 ##################################################################
 #########################Example header###########################
 ##################################################################
-#!/bin/sh 
+#!/bin/sh
 
 ##choose queue
-####PBS -q 
+####PBS -q
 ##list - node are nodes: ppn are cpus per node: walltime=walltime
-#PBS -l nodes=1:ppn=1:mem=10gb,walltime=30:00
+#PBS -l nodes=1:ppn=1,mem=10gb,walltime=30:00
 ##email
 #PBS -M fake_email@auburn.edu
 ##send email abort; begin; end
-#PBS -m ae 
+#PBS -m ae
 ##job name
 #PBS -N sra_test
 ##combine standard out and standard error
-#PBS -j oe 
+#PBS -j oe
 ##################################################################
 ##################################################################
 ##################################################################
@@ -49,10 +49,10 @@ HPC_PBS_Header.py email@whatever.com yourscript.sh
 
 import sys
 
-email=sys.argv[1]
-output_file_name=sys.argv[2]
+email = sys.argv[1]
+output_file_name = sys.argv[2]
 
-shebang="#!/bin/bash"
+shebang = "#!/bin/bash"
 
 nodes = raw_input("number of nodes: ")
 ppn = raw_input("processors per node: ")
@@ -60,11 +60,10 @@ mem = raw_input("memory per cpu: ")
 walltime = raw_input("Walltime (hh:mm:ss): ")
 jobname = raw_input("Job Name: ")
 
-with open(output_file_name, "w") as out: 
-	out.write("%s \n" % shebang)
-	out.write("#PBS -l nodes=%s:ppn=%s:mem=%s,walltime=%s \n" % (nodes, ppn, mem, walltime))
-	out.write("#PBS -M %s \n" % (email))
-	out.write("#PBS -m ae \n")
-	out.write("#PBS -N %s \n" % (jobname))
-	out.write("#PBS -j oe \n")
-
+with open(output_file_name, "w") as out:
+        out.write("%s \n" % shebang)
+        out.write("#PBS -l nodes=%s:ppn=%s,mem=%s,walltime=%s \n" % (nodes, ppn, mem, walltime))
+        out.write("#PBS -M %s \n" % (email))
+        out.write("#PBS -m ae \n")
+        out.write("#PBS -N %s \n" % (jobname))
+        out.write("#PBS -j oe \n")
