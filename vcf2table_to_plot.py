bd512758ed0e1b103f4cf81effae3eb9d8626a18
ssefick <ssefick@gmail.com>
2017-03-14 09:08:18 -0500

    script documented

--- vcf2table_to_plot.py
+++ vcf2table_to_plot.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 
+'''
+Author: Stephen A. Sefick
+Date: 20170314
+Language: Python 2.7
+
+Usage:
+vcf2table_to_plot.py SNP.vcf SNP_table.out.txt
+
+
+This script will take a SNP only vcf and strip out a number of annotations 
+that may be of interest to plot and decide on hard filtering
+'''
+###########################################################################
+##imports
 import sys
 import csv
 import vcf
+###########################################################################
 
+###########################################################################
+##commands
 ##open both input and output files
 ##this automatically closes the files when the script is done
 ##with open('test3.vcf', 'r') as infile, open('test_out_py', 'w') as out_csv:
@@ -20,3 +37,4 @@ with open(infile, 'r') as infile, open(outfile, 'w') as out_csv:
             GQ_DP_SAMPLE=[j.sample, str(j['GQ']), str(j['DP']), str(i.INFO['FS']), str(i.INFO['DP']), str(i.INFO['QD'])]
             ##out_str=",".join(GQ_DP_SAMPLE)
             out.writerow(GQ_DP_SAMPLE)
+###########################################################################
