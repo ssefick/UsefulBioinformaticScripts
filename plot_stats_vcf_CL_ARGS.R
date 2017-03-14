82599cdf0c42e059daabed278fe1a17b4161a264
ssefick <ssefick@gmail.com>
2017-03-14 09:02:00 -0500

    python script and R plot script

--- plot_stats_vcf_CL_ARGS.R
+++ plot_stats_vcf_CL_ARGS.R
@@ -1,31 +1,31 @@
-#!/usr/bin/env Rscript
+>#!/usr/bin/env Rscript
 
 ################################################################################
 
 require(argparse)
 
 parser <- ArgumentParser()
-# specify our desired options
-# by default ArgumentParser will add an help option
+                                        # specify our desired options
+                                        # by default ArgumentParser will add an help option
 
 
 parser$add_argument("-i", "--input_vcf", action="store", default="Table from VCF file", dest="input_vcf",
- help="Input %(default)s")
+                    help="Input %(default)s")
 
 parser$add_argument("-o", "--output_pdf", action="store", default="out.pdf", dest="output_pdf",
- help="Output %(default)s")
+                    help="Output %(default)s")
 
 parser$add_argument("-DP", "--Depth_cut_off", action="store", default="TRUE", dest="raw_DP",
- help="TRUE/FALSE; Truncate graph to 5*SD; defaults to  %(default)s")
+                    help="TRUE/FALSE; Truncate graph to 5*SD; defaults to  %(default)s")
 
 
-# get command line options, if help option encountered print help and exit,
-# otherwise if options not found on command line then set defaults,
+                                        # get command line options, if help option encountered print help and exit,
+                                        # otherwise if options not found on command line then set defaults,
 argv <- parser$parse_args()
 
 ################################################################################
 
-#test
+                                        #test
 
 test <- 0
 
@@ -44,27 +44,27 @@ input <- argv["input_vcf"][[1]]
 output <- argv["output_pdf"][[1]]
 raw_DP <- argv["raw_DP"][[1]]
 
-#libraries
+                                        #libraries
 require(ggplot2)
 require(grid)
 require(gtable)
 require(gridExtra)
 
 
-#get data in
+                                        #get data in
 x <- read.table(input, sep="\t", header=TRUE)
 
-#plot FS data
+                                        #plot FS data
 FS.plot <- ggplot(x, aes(x=FS))+geom_density(alpha=0.2)
 FS.plot <- FS.plot + scale_x_log10() + geom_vline(xintercept = 60, colour="red")
 FS.plot <- FS.plot + ggtitle("Fisher Strand (FS- Strand Bias Variant on F or R)")
 
-#plot QD
+                                        #plot QD
 QD.plot <- ggplot(x, aes(x=QD))+geom_density(alpha=0.2)
 QD.plot <- QD.plot + geom_vline(xintercept = 2, colour="red")
 QD.plot <- QD.plot + ggtitle("QD -variant qual/unfiltered depth")
 
-#plot DP
+                                        #plot DP
 DP.plot <- ggplot(x, aes(x=DP))
 DP.plot <- DP.plot + geom_density(alpha=0.2)
 DP.plot <- DP.plot + ggtitle("DP- Depth")
@@ -90,8 +90,8 @@ DP.plot
 ##table Depth Stats
 ############################################################################
 
-#old table
-#grid.arrange(tableGrob(stats))
+                                        #old table
+                                        #grid.arrange(tableGrob(stats))
 
 t1 <- tableGrob(stats)
 title <- textGrob("Depth Statistics",gp=gpar(fontsize=50))
@@ -105,3 +105,4 @@ grid.draw(table)
 ############################################################################
 
 dev.off()
+
